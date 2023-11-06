169c95e2790a07aaef1c870a74d7e4bcfeeecc69
srinutech811 <149927578+srinutech811@users.noreply.github.com>
2023-11-06 09:52:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My parenet New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 &  Blueberrry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range egg')
+streamlit.text('🥣 Omega 3 &  Blueberrry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range egg')
+streamlit.text('🥑🍞Hard-Boiled Free-Range egg')
