59197a16db3742a261b4907d4d70d6feeb8dc942
srinutech811 <149927578+srinutech811@users.noreply.github.com>
2023-11-06 10:11:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import panas
 
 streamlit.title('My parenet New Healthy Dinner')
 streamlit.header('Breakfast Menu')
@@ -7,3 +8,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range egg')
 streamlit.text('🥑🍞Hard-Boiled Free-Range egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
