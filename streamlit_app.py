865a052e76d9a78c8976f159039602e181cdf111
srinutech811 <149927578+srinutech811@users.noreply.github.com>
2023-11-07 17:38:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,5 +37,5 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
-
+streamlit.dataframe(my_data_row)
 
